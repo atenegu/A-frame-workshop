7faf58f36c68b329b428a93a29be68ee98598d03
atenegu <atene.gu@gmail.com>
2021-06-25 23:32:02 +0200

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -28,13 +28,12 @@ myOtherBox.addEventListener('mouseleave', function(){
  * click-events can use a fuse (default on mobile). So the event only fires, if the cursor is on the object for a specified time.
  */
 
-var growspeed = 1.2;
+var growspeed = 5;
 
 function grow(){
 	myOtherBox.object3D.scale.x *= growspeed;
 	myOtherBox.object3D.scale.y *= growspeed;
 	myOtherBox.object3D.scale.z *= growspeed;
-	myOtherBox.object3D.rotation.x *= rotationSpeed*10;
 	//console.log(myOtherBox.object3D.scale);
 }
 
